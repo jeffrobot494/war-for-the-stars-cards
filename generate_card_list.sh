3b37ef2014d2e3f9fcae9bf7f43f6cc9733fcb26
Bo Banducci <bobanducci90@gmail.com>
2025-02-05 15:03:41 -0600

    Auto-update: 2025-02-05 15:03:40

--- generate_card_list.sh
+++ generate_card_list.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 # Generate a JSON list of PNG filenames manually
 echo "[" > cards.json
-ls cards/*.png | awk '{ print "  \"" $0 "\"," }' >> cards.json
+ls cards/*.png | sed 's|cards/||' | awk '{ print "  \"" $0 "\"," }' >> cards.json
 sed -i '$ s/,$//' cards.json  # Remove the trailing comma on the last line
 echo "]" >> cards.json
 
